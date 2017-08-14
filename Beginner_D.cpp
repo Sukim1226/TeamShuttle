77900983b7dbc7450849fb72f1c9ab46f8926c33
hipiphock <hipiphock@naver.com>
2017-08-14 14:28:20 +0900

    wtf

--- Beginner_D.cpp
+++ Beginner_D.cpp
@@ -1,3 +1,8 @@
+/*
+	hingook
+	beginner D
+*/
+
 #include <iostream>
 #include <algorithm>
 
@@ -27,15 +32,31 @@ int main(){
 		for(int j = 1; j<=m; j++)
 			scanf("%d", &arr[i][j]);
 	
-	arr[0][1] = arr[1][0] = -1;
+	arr[0][1] = arr[1][0] = 500000;
+	
 	for(int i = 1; i<=n; i++)
 		for(int j = 1; j<=m; j++)
-			if(arr[i - 1][j] == 0 && arr[i][j - 1] == 0)
-				dp[i][j] == 0;
+			if(arr[i - 1][j] == 0 && arr[i][j - 1] == 0){
+				arr[i][j] = 0;
+			}
+			
+	arr[0][1] = arr[1][0] = 0;
 	
-	for(int i = 2; i<=n; i++)
-		for(int j = 2; j<=m; j++)
-			dp[i][j] = max(arr[i - 1][j], arr[i][j - 1]) + arr[i][j];
+	for(int i = 1; i<=n; i++)
+		for(int j = 1; j<=m; j++){
+			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + arr[i][j];
+			if(i != 1 && j != 1)
+				if(arr[i - 1][j] == 0 && arr[i][j - 1] == 0 && arr[i - 1][j - 1] == 0)
+					dp[i][j] = 0;
+		}
+		
+	/*
+	printf("\n");
+	for(int i = 1; i<=n; i++){
+		for(int j = 1; j<=m; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
 	
 	printf("\n");
 	for(int i = 1; i<=n; i++){
@@ -43,6 +64,7 @@ int main(){
 			printf("%d ", dp[i][j]);
 		printf("\n");
 	}
+	*/
 
 	printf("%d", dp[n][m]);
 	return 0;
