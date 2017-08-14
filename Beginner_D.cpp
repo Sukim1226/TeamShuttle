3d8ef84043589a69c75b066bdfd098500c2a5730
hipiphock <hipiphock@naver.com>
2017-08-14 15:03:55 +0900

    sukim help

--- Beginner_D.cpp
+++ Beginner_D.cpp
@@ -31,39 +31,46 @@ int main(){
 	for(int i = 1; i<=n; i++)
 		for(int j = 1; j<=m; j++)
 			scanf("%d", &arr[i][j]);
-	
-	arr[0][1] = arr[1][0] = 500000;
-	
-	for(int i = 1; i<=n; i++)
-		for(int j = 1; j<=m; j++)
-			if(arr[i - 1][j] == 0 && arr[i][j - 1] == 0 && arr[i - 1][j - 1] == 0)
-				arr[i][j] = 0;
 			
-	arr[0][1] = arr[1][0] = 0;
+	arr[0][0] = 100000;
 	
-	for(int i = 1; i<=n; i++)
-		for(int j = 1; j<=m; j++){
-			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + arr[i][j];
-			if(i != 1 && j != 1)
-				if(arr[i - 1][j] == 0 && arr[i][j - 1] == 0 && arr[i - 1][j - 1] == 0)
-					dp[i][j] = 0;
-		}
-		
-	
-	printf("\n");
 	for(int i = 1; i<=n; i++){
-		for(int j = 1; j<=m; j++)
+		for(int j = 1; j<=m; j++){
+			if(!arr[i - 1][j - 1] && !arr[i - 1][j] && !arr[i][j - 1])
+				arr[i][j] = 0;
+		}
+	}
+	
+	arr[0][0] = 0;
+	
+	for(int i = 1; i<=n; i++){
+		for(int j = 1; j<=m; j++){
+			if(!arr[i][j])
+				dp[i][j] = 0;
+			else{
+				if(!arr[i - 1][j] && !arr[i][j - 1])
+					dp[i][j] = arr[i - 1][j - 1] + arr[i][j];
+				else
+					dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + arr[i][j];
+			}
+		}
+	}
+	
+	/*
+	printf("\n");
+	for(int i = 0; i<=n; i++){
+		for(int j = 0; j<=m; j++)
 			printf("%d ", arr[i][j]);
 		printf("\n");
 	}
 	
 	printf("\n");
-	for(int i = 1; i<=n; i++){
-		for(int j = 1; j<=m; j++)
+	for(int i = 0; i<=n; i++){
+		for(int j = 0; j<=m; j++)
 			printf("%d ", dp[i][j]);
 		printf("\n");
 	}
-	
+	*/
 
 	printf("%d", dp[n][m]);
 	return 0;
