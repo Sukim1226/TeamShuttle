2a9059e4c2f20dbd800e04eef4e06f71040d5992
hipiphock <hipiphock@naver.com>
2017-08-21 21:03:12 +0900

    ALL SOLVED

--- Beginner_E.cpp
+++ Beginner_E.cpp
@@ -3,35 +3,35 @@
 
 using namespace std;
 
-int main(){
+int main() {
 	string s;
 	string brain;
-	cin>>s>>brain;
-	string tmp;
-	int i = 0, j1 = 0, j2 = s.length() - 1;
+	cin >> s >> brain;
+	int i = 0;
 	bool reverse = true;	//true가 순방향, false가 역방향 
-	while(i != brain.length()){
-		if(brain[i] == 'S'){
-			if(reverse = true){
-				cout<<s.front();
+	while (i != brain.length()) {
+		if (brain[i] == 'S') {
+			if (reverse) {
+				cout << s.front();
 				s.erase(s.begin());
 			}
-			else{
-				cout<<s.back();
-				s.erase(s.end());
+			else {
+				cout << s.back();
+				//s.erase(s.end());
+				s.pop_back();
 			}
 		}
-		if(brain[i] == 'R'){
-			if(reverse)
+		if (brain[i] == 'R') {
+			if (reverse)
 				reverse = false;
 			else
-				reverse = true;			
+				reverse = true;
 		}
-		if(brain[i] == 'A'){
-			if(reverse){
+		if (brain[i] == 'A') {
+			if (reverse) {
 				s = "x" + s;
 			}
-			else{
+			else {
 				s = s + "x";
 			}
 		}
