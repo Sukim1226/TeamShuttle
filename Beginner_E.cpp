a2bf50d450b0c68d01b52909525c6921bfe227cf
hipiphock <hipiphock@naver.com>
2017-08-21 20:06:28 +0900

    shit

---
+++ Beginner_E.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string s;
+	string brain;
+	cin>>s>>brain;
+	string tmp;
+	int i = 0, j1 = 0, j2 = s.length() - 1;
+	bool reverse = true;	//true가 순방향, false가 역방향 
+	while(i != brain.length()){
+		if(brain[i] == 'S'){
+			if(reverse = true){
+				cout<<s.front();
+				s.erase(s.begin());
+			}
+			else{
+				cout<<s.back();
+				s.erase(s.end());
+			}
+		}
+		if(brain[i] == 'R'){
+			if(reverse)
+				reverse = false;
+			else
+				reverse = true;			
+		}
+		if(brain[i] == 'A'){
+			if(reverse){
+				s = "x" + s;
+			}
+			else{
+				s = s + "x";
+			}
+		}
+		i++;
+	}
+	return 0;
+}
